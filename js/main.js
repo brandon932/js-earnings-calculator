a7e8b2784e2f6049e7d04ea4bb7eca6486298a07
KyleRattet <krattet@gmail.com>
2015-08-05 10:14:33 -0600

    refactored for OPP and dry code

--- js/main.js
+++ js/main.js
@@ -1,44 +1,27 @@
 // add scripts
-
 $(document).on('ready', function() {
 
+  var valuesArr = [];
 
-var valuesArr = [];
+  $(':submit').on('click', function (e) {
+    e.preventDefault();
+    valuesArr=[];
+    var formValues = $('.form-input');
+    for (var i = 0; i < formValues.length; i++) {
+      valuesArr.push($($('.form-input')[i]).val());
+      $($('.form-input')[i]).val("");
+    }
+    var mealData= new Calculate(valuesArr)
+    $('#subtotal').html(" $" + mealData.subTotal());
+    $('#tip').html(" $" +mealData.tip());
+    $('#total').html(" $" +(mealData.subTotal()+mealData.tip()) );
+  });
 
-$(':submit').on('click', function (e) {
-  e.preventDefault();
-  valuesArr=[];
-  var formValues = $('.form-input');
-  for (var i = 0; i < formValues.length; i++) {
-    valuesArr.push($($('.form-input')[i]).val());
-    $($('.form-input')[i]).val("");
-  }
-
-  console.log(valuesArr);
-  // return valuesArr;
-  var subTotal = subTotalPrice(valuesArr);
-  $('#subtotal').html(" $" +subTotal);
-
-  var tipTotal = tipPrice(valuesArr);
-  $('#tip').html(" $" +tipTotal);
-
-  $('#total').html(" $" + totalCharges(subTotal, tipTotal));
-
+  $(':button').on('click', function (e) {
+    e.preventDefault();
+    var formValues = $('.form-input');
+    for (var i = 0; i < formValues.length; i++) {
+      $($('.form-input')[i]).val("");
+    }
+  });
 });
-
-$(':button').on('click', function (e) {
-  e.preventDefault();
-   var formValues = $('.form-input');
-  for (var i = 0; i < formValues.length; i++) {
-    $($('.form-input')[i]).val("");
-  }
-});
-
-
-
-});
-
-
-
-
-
--- js/utility.js
+++ js/utility.js
@@ -1,26 +1,13 @@
-
-function subTotalPrice (valuesArr) {
-
-  var mealPrice = parseInt(valuesArr[0]);
-  var taxRate = valuesArr[1];
-  var tipPercetage = valuesArr[2];
-
-  var subTotal = mealPrice + (mealPrice*taxRate);
-
-  return subTotal;
+var Calculate = function(valuesArr) {
+  this.mealPrice = parseInt(valuesArr[0]);
+  this.taxRate = valuesArr[1];
+  this.tipPercetage = valuesArr[2];
 }
-
-function tipPrice (valuesArr) {
-
-  var mealPrice = parseInt(valuesArr[0]);
-  var taxRate = valuesArr[1];
-  var tipPercetage = valuesArr[2];
-
-  var tipTotal = mealPrice*tipPercetage;
-
-  return tipTotal;
+Calculate.prototype.subTotal = function(){
+  subTotalPrice = this.mealPrice + (this.mealPrice*(this.taxRate))
+  return subTotalPrice;
 }
-
-function totalCharges (subTotal, tipTotal) {
-  return subTotal + tipTotal;
+Calculate.prototype.tip = function (){
+  tip = this.tipPercetage * this.mealPrice;
+  return tip;
 }
