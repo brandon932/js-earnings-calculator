3f30582b7e9620055e427c02f3a9a0440f66f200
KyleRattet <krattet@gmail.com>
2015-08-05 11:29:56 -0600

    Updated running totals

--- js/main.js
+++ js/main.js
@@ -2,19 +2,24 @@
 $(document).on('ready', function() {
 
   var valuesArr = [];
-
+  var tipArr = [];
   $(':submit').on('click', function (e) {
     e.preventDefault();
     valuesArr=[];
     var formValues = $('.form-input');
+    // TODO: add check for form - imput === ""
     for (var i = 0; i < formValues.length; i++) {
       valuesArr.push($($('.form-input')[i]).val());
       $($('.form-input')[i]).val("");
     }
-    var mealData= new Calculate(valuesArr)
+    var mealData= new Calculate(valuesArr, tipArr)
+    tipArr.push(mealData.tip());
     $('#subtotal').html(" $" + mealData.subTotal());
     $('#tip').html(" $" +mealData.tip());
     $('#total').html(" $" +(mealData.subTotal()+mealData.tip()) );
+    $('#totalTips').html(" $" +mealData.addTips());
+    $("#mealCount").html(" " + tipArr.length);
+    $("#averageTip").html(" $" + (mealData.addTips() / tipArr.length))
   });
 
   $(':button').on('click', function (e) {
--- js/utility.js
+++ js/utility.js
@@ -1,7 +1,8 @@
-var Calculate = function(valuesArr) {
+var Calculate = function(valuesArr, tipArr ) {
   this.mealPrice = parseInt(valuesArr[0]);
   this.taxRate = valuesArr[1];
   this.tipPercetage = valuesArr[2];
+  this.totalTips = tipArr;
 }
 Calculate.prototype.subTotal = function(){
   subTotalPrice = this.mealPrice + (this.mealPrice*(this.taxRate))
@@ -10,4 +11,11 @@ Calculate.prototype.subTotal = function(){
 Calculate.prototype.tip = function (){
   tip = this.tipPercetage * this.mealPrice;
   return tip;
+}
+Calculate.prototype.addTips = function(){
+  var total = 0;
+  for (var i = 0; i < this.totalTips.length; i++) {
+      total += this.totalTips[i]
+  }
+  return total;
 }
